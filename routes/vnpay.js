2ac1486f8ffac41de89699ff988f7860200a53fd
vanh2753 <vanh2753@gmail.com>
2025-06-15 21:47:44 +0700

    fix route

--- routes/vnpay.js
+++ routes/vnpay.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { createPaymentUrl, handleIPN, handleReturn } = require('../controllers/vnpay-controller');
 
-router.get('/create-payment/:orderId', createPaymentUrl);
-router.get('/callback', handleIPN); // <-- IPN URL: VNPAY gọi đến
-router.get('/return', handleReturn); // <-- Return URL: user được redirect về
+router.get('/vnpay/callback', handleIPN); // <-- IPN URL: VNPAY gọi đến
+router.get('/vnpay/return', handleReturn); // <-- Return URL: user được redirect về
+router.get('/vnpay/create-payment/:orderId', authenticateToken, createPaymentUrl);
 
 module.exports = router;
